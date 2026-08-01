75da4e282049f6358c477afedadcad3fc3fe786f
Calvin <calvinchristianho2@gmail.com>
2026-08-02 01:20:42 +0700

    Add Temporary page.js

---
+++ src/app/seller/edit-product/page.js
@@ -0,0 +1,5 @@
+export default function EditProductPage() {
+    return(
+        <div>Edit Product Page</div>
+    )
+}
---
+++ src/app/seller/products/page.js
@@ -0,0 +1,5 @@
+export default function SellerProductsPage() {
+	return (
+		<div>Seller Products Page</div>
+	)
+}
